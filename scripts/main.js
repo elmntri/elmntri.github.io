d3ab263e347a8d305961168cb92a1849b882168a
Alex Lau(AvengerMoJo) <alex.lau@elmntri.com>
2023-10-10 18:24:15 +0800

    Adding a proxy https server
    Signed-off-by: Alex Lau(AvengerMoJo) <alex.lau@elmntri.com>

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ var submit_sample = function(){
     var req_type = "text";
     var textarea = document.getElementById("test_textarea");
     var txt = textarea.value;
-    const apiUrl = 'http://bern2.korea.ac.kr/plain';
+    const apiUrl = 'http://128.199.167.86:443';
+    // const apiUrl = 'http://bern2.korea.ac.kr/plain';
     // Create the JSON payload
     const payload = {
       text: txt
